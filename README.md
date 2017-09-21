798f0c228b5a02fbf8f18df0e1e6aabf2ce7bf90
Arturo Salmeron <asalmeron3@gatech.edu>
2017-09-20 22:21:47 -0400

    Readme and CSS

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # GiphyAPI_hw - week's 6 api and ajax
 
-I'll be back...
+Overview: this webiste will have a section for buttons (each with topics), a section for inputing text that will create a new button, a section to store 10 gifs related to the topic of the button clicked. 
+
+--------------Breaking it down into steps----------------
+
+Creating a wesite that:
+
+	1) Contains a list of buttons at the top of the page. These buttons:
+		a. Contains the name of a tv show (or a topic) for the user to click on
+		b. The names of show/topics of the initial buttons is stored in an array
+		c. a separate function goes through the array and makes the buttons to append to the top of the web page
+		d. upon clicking the button ....(Section 3)
+
+
+	2.Contains a section of the user to create a button that fucntions in the same manner as the buttons originally on the page. This is done by:
+		a. allowing the user to input text into a "form"
+		b. this text is submitted via a submit button
+		c. the form should NOT  clear/refresh the page (thus, disable the  default option to the form)
+		d. the text is added to the original array AND 
+		e. the function that creates the buttons runs again, this time with the user's input being created into a button
+		f. ADDED/Bonus: if the user has already created or has a button for a certain topic, or if the user left the form blank, NO button will be created
+
+	3. Upon Clicking the button to a topic:
+		a. An ajax call should be made to the Giphy API for 10 gifs.
+			i. From the API's response, we want to have the Gifs static link, the Gif's automated link, and the rating to the link
+		b. The 10 gifs should be prepended/inserted into the third (3rd) section on the left, that contains all the still Gifs
+		c. In this case, each row will have 3 gifs
+
+	4.Upon Clikcing on the GIFs:
+		a. If the gif is still, the gif should be animated using the animated link information stored in the div's attribute
+		b. If the gif is animated, the gif should be made still by using the still-link information stored in the div's attribute
--- assets/javascript/generate.js
+++ assets/javascript/generate.js
@@ -5,12 +5,13 @@
 
   var numOfGifs = 10; //number of gif results per topic...for later use
 
+  var sectionNum = 0; //I am making this variable called section number. I want to be able to track groups of 10 gifs. Once the GifSection has 20 gifs, I want to delete the previous 10 gifs but add the next 10
 
 function MakeButtons(){
   //loop through the array of topics and create buttons for each one
   for (i = 0; i < buttonsArray.length; i++){
     //create & append a button to div "theButtonsAtTheTop" and add the class to each of the button and the data attribute that corresponds to the topic from the buttonsArray
-    $(".theButtonsAtTop").append($("<button>").addClass("topicToSearch").attr("data-name", buttonsArray[i]).text(buttonsArray[i]));
+    $(".theButtonsAtTop").append($("<button>").addClass("btn btn-default btn-primary topicToSearch").attr("data-name", buttonsArray[i]).text(buttonsArray[i]));
   }
 
 }
@@ -22,11 +23,13 @@ MakeButtons();
 
   //make a function that runs when the user clicks on the sumbit-button. THe function should take the input typed and push it to the buttonsArray AND also create a button to the buttonsAtTheTop div
   $("#clickToAddButton").on("click", function(event){
+    $("#userGifSearch").empty();
+    //prevent the submit button from clearing 
     event.preventDefault();
     $(".theButtonsAtTop").empty();
     //obtain & store the text that the user submitted
     var userInput = ($("#userGifSearch").val().trim()).toLowerCase();
-    //check to make sure the input is not already a button
+    //check to make sure the input is not already a button in the array
     if(buttonsArray.indexOf(userInput) ==-1 && userInput!= ""){
       $(".theButtonsAtTop").empty();
       //add this text/string to the buttonsArray
@@ -37,70 +40,91 @@ MakeButtons();
 
 
 
+//----------------------Use Ajax to get the Giphy API-----------------------
 
     // Make a variable to store you api key
     var apiKey = "56f6f3a6e79e4454ba6f3f22bef87372";
 
 
-    //upon the user typing and submitting their search,
+    //if the user types in some text for a new button, AND clicks on the button to submit, 
     $(document).on("click", ".topicToSearch", function(){
 
       var topic = $(this).attr("data-name");
       //make a variable to store the link to the giphy api AND appends the api Key to the link and the topic (the data-name) from the button clicked AND limit the results to 6
-      // using httpS
       var queryURL = "https://api.giphy.com/v1/gifs/search?&limit=" + numOfGifs + "&api_key=" + apiKey + "&q=" + topic;
+      
+      sectionNum++;
 
-      //use the method ajax to search the api for the gifs we want to see
+
+      //use the method "ajax" to search the api for the gifs we want to see
       $.ajax({
         url: queryURL,
         method: "GET" 
       }).done(function(gifResults) {
-    
+        
         for (j = 0; j < numOfGifs; j++){
-          //create a small individual img tag for the static gif. Add the link to the static image (as the src), add a data attribute that contains the link to a still image, another for an animated image, a class, text that will show the gif's rating, and a data attribute that tells whether the image is currently animated or still
+          //create a div that will store the image and the text under the image. this div will also be stored in the "theLeftGifContainer" div
           var oneGifContainer = $("<div>");
-            oneGifContainer.addClass("col-xs-4 col-sm-4 col-md-4 col-lg-4 gifBlock")
+          // for 3 images per row, the width of each div should be 4 (12 / 3 = 4) [12 because all parent divs in bootstrap are 12 columns wide]
+            oneGifContainer.addClass("col-xs-4 col-sm-4 col-md-4 col-lg-4  gifBlock");
+            oneGifContainer.attr("id", "Group-" + sectionNum);
 
+          //create a small individual img tag for the static gif  . 
           var theImage = $("<img>")
-
+            //Add the link to the static image (as the src)
             theImage.attr("src", gifResults.data[j].images.fixed_height_small_still.url);
-
+            //, add a data attribute that contains the link to a still image,
             theImage.attr("dataIfStill",gifResults.data[j].images.fixed_height_small_still.url);
-
+            // another for an animated image
             theImage.attr("dataIfAnimated",gifResults.data[j].images.fixed_height_small.url); 
-
+            //data attribute that tells whether the image is currently animated or still
             theImage.attr("currently", "still");
-
+            //, a class for entire div (useful for later when we want to click on div)
             theImage.addClass("gif");
 
+          // Create a paragraph tag that will show/contain the gif's rating
           var theRating = $("<p>").text("Rating: " + gifResults.data[j].rating);
 
+          //append the paragraph tag and the image to your OneGifContainer
           var theImgNtheRating = oneGifContainer.append(theImage).append(theRating);
 
+          //append the single "OneGifContainer" to theLeftGifContainer (the left container hold all of our static gif; 3 per row)
           $(".theLeftGifContainer").prepend(theImgNtheRating);
 
+          if (sectionNum >2){
+            $("#Group-" + (sectionNum-1)).remove();
+          }
+
         }
-
-
         
       });
 
+    });
 
 
-    })
 
+
+    //-----------------------Clicking on a Gif--------------------------
+    
+    //query the entire document for when the user clicks on a gif (with the class "gif")
     $(document).on("click",".gif", function(){
 
+      //check the clicked on gif's attribute called "currently" and store that attribute in a variable (for comparing)
       var state = $(this).attr("currently");
-
+        //if the attribute is "still"...
         if (state == "still"){
+          //change the "src" to the animated link stored in the "dataIfAnimated" attibute
           $(this).attr("src", $(this).attr("dataIfAnimated"));
+          //also change the "currently" attribute to animate (as it will now be animated)
           $(this).attr("currently", "animate");
 
         }
-        // and update the data-state attribute to 'animate'.
+        //if the attribute is "animated...
+
         else if (state == "animate"){
+          //revert the "src" back to a still image
           $(this).attr("src", $(this).attr("dataIfStill"));
+          //revert the attribute named "currently", back to "still"
           $(this).attr("currently", "still");
 
         }
